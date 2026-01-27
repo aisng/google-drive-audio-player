51620b45fe07327e045499d72d57da433b4a0a08
rupuin <tasignas@gmail.com>
2026-01-27 19:34:48 +0100

    remove old packages

--- app/audio_player/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AudioPlayerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "audio_player"
-
-    def ready(self):
-        from .signals import create_profile, save_profile, delete_user
