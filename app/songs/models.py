4e9ef1e5a9b2d772ea1dac60ba093a684a45b382
rupuin <tasignas@gmail.com>
2026-01-27 19:33:49 +0100

    create songs pkg

---
+++ app/songs/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Song(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    title = models.CharField("Title", max_length=150)
+    path = models.CharField("Path", max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="replies",
+    )
+    song = models.ForeignKey(
+        Song, on_delete=models.SET_NULL, null=True, blank=True, to_field="id"
+    )
+    body = models.TextField("Body", max_length=1000, help_text="Comment body")
+    timestamp = models.CharField("Timestamp", max_length=5, null=True, blank=True)
+    date_created = models.DateTimeField("Created at", auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    @property
+    def short_body(self):
+        return f"{self.body}" if len(self.body) <= 30 else f"{self.body[:30]}..."
+
+    def get_replies(self):
+        return self.replies.all()
+
+    def __str__(self):
+        return f"'{self.short_body}' / {self.user} / {self.id} "
